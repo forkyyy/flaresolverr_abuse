7777bdb9d4b16ca37aad0fc85630236fc390aa01
forkyyy <67451588+forkyyy@users.noreply.github.com>
2023-07-12 19:16:00 -0300

    Create scraper.js

---
+++ scraper.js
@@ -0,0 +1,31 @@
+const axios = require('axios');
+
+const baseUrl = 'https://search.censys.io/api/v2/hosts/search';
+const headers = {
+  'Accept': 'application/json',
+  'Authorization': 'Basic YTcxYTg0NTAtZmU5NC00MjVjLWIwNTQtMzFkYjc3NWUyZDViOmFaVDJ6ejdoeFRVMVlqSFd6d2VnNzdDTHFKMGVjelVu'
+};
+
+function sendRequest(url) {
+    axios.get(url, { headers })
+        .then(response => {
+        const { data } = response;
+        const { result } = data;
+        const { hits } = result;
+
+        for (const hit of hits) {
+            console.log(`${hit.ip}`);
+        }
+
+        if (result.links && result.links.next) {
+            const nextUrl = `${url}&cursor=${result.links.next}`;
+            sendRequest(nextUrl);
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error.message);
+    });
+}
+
+const initialUrl = `${baseUrl}?q=%28FlareSolverr%29%20and%20services.service_name%3D%60HTTP%60&per_page=100&virtual_hosts=EXCLUDE`;
+sendRequest(initialUrl);
